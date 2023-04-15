6bdfc5523bb9bf6043ad75e47d12f938298a7e08
elonner <elilnnr5@gmail.com>
2023-04-15 15:28:41 -0700

    makes JS more object-oriented, cleans up code, adds comments, tries animation again

--- js/script.js
+++ js/script.js
@@ -1,44 +1,58 @@
-//--------constants
-const DISPLAY = {
-    'X': ['X', 'https://i.imgur.com/gG6q8U7.png'],
-    'O': ['O', 'https://i.imgur.com/wB9i5bI.png'],
-}
-
 //--------state variables
 let board;
 let turn;
 let winner;
 let totTurns;
 
+//--------object/classes
+class Cell {
+    constructor(value, element) {
+        this.value = value;
+        this.element = element;
+        this.element.classList.remove(...this.element.classList);
+        this.element.classList.add('hoverable');
+        this.imgURL;
+    }
+}
+
 
 //--------cached elements
 const messageEl = document.querySelector('h1');
 const playAgainBtn = document.querySelector('button');
-const tileEls = document.querySelectorAll('#board > div');
+const xIconEl = document.getElementById('xIcon');
+const oIconEl = document.getElementById('oIcon');
 
 //--------event listeners
 document.getElementById('board').addEventListener('click', handleMove);
 playAgainBtn.addEventListener('click', init);
 
 
-//--------functions
-init();
+//--------functions;
+init(); 
 
 // Initialize all state, then call render()
 function init() {
 
+    //create board of rows
     board = [
-        [0, 0, 0], //row 0
-        [0, 0, 0], //row 1
-        [0, 0, 0]  //row 2
+        [], //row 0
+        [], //row 1
+        []  //row 2
     ];
 
-    tileEls.forEach(tile => {
-        if (tile.firstChild) {
-            tile.removeChild(tile.firstChild);
-            tile.classList.remove('full');
+    //add 3 empty Cells per row and grab the corresponding HTML element for cell.imgURL
+    board.forEach((row, rowIdx) => {
+        for (let i = 0; i < 3; i++) {
+            const cellId = `c${i}r${rowIdx}`;
+            const cellEl = document.getElementById(cellId);
+            //remove any added classes
+            cellEl.classList.remove(...cellEl.classList);
+            //remove any unwanted children
+            cellEl.innerHTML = '';
+            row.push(new Cell(0, cellEl));
         }
     });
+
     turn = 'X';
     totTurns = 0;
     winner = null;
@@ -48,40 +62,49 @@ function init() {
 function handleMove(e) {
     //ensure click is on correct element
     if (e.target.tagName.toLowerCase() !== 'div' || winner) return;
-    //update board
-    const rowIdx = e.target.id[3];
-    const colIdx = e.target.id[1];
-    //change value of square
-    if (board[rowIdx][colIdx] === 'X' || board[rowIdx][colIdx] === 'O') return;
-    board[rowIdx][colIdx] = turn;
-    totTurns++;
-     //switch turns
+    //store useful data
+    const clickedCell = board[e.target.id[3]][e.target.id[1]];
+    //if not already taken with X/O change value of cell and update cell object
+    if (clickedCell.value === 'X' || clickedCell.value === 'O') return;
+    clickedCell.value = turn; //X or O
+    //add icon images
+    if (turn ===  'X') { 
+        clickedCell.imgURL = 'https://i.imgur.com/gG6q8U7.png';
+    } else {
+        clickedCell.imgURL = 'https://i.imgur.com/wB9i5bI.png';
+    }
+    //switch turns
     turn === 'X' ? turn = 'O' : turn = 'X';
+    totTurns++;
     //check for winner
     winner = getWinner();
     render();
 }
 
+//returns the value of the winner (X/O/T or null)
 function getWinner() {
     //check rows
-    for (let row of board) {
-        if (new Set(row).size === 1) return row[0];
+    for (let i = 0; i < 3; i++) {
+        const row = new Set([board[i][0].value, board[i][1].value, board[i][2].value]);
+        if (!row.has(0) && row.size === 1) {
+            return board[i][0].value;
+        }
     }
     //check columns
     for (let i = 0; i < 3; i++) {
-        const col = new Set([board[0][i], board[1][i], board[2][i]]);
-        if (col.size === 1) {
-            return board[0][i];
+        const col = new Set([board[0][i].value, board[1][i].value, board[2][i].value]);
+        if (!col.has(0) && col.size === 1) {
+            return board[0][i].value;
         }
     }
     //Check diagonals
-    const diag1 = new Set([board[0][0], board[1][1], board[2][2]]);
-    if (diag1.size === 1) {
-        return board[1][1];
+    const diag1 = new Set([board[0][0].value, board[1][1].value, board[2][2].value]);
+    if (!diag1.has(0) && diag1.size === 1) {
+        return board[1][1].value;
     }
-    const diag2 = new Set([board[0][2], board[1][1], board[2][0]]);
-    if (diag2.size === 1) {
-        return board[1][1];
+    const diag2 = new Set([board[0][2].value, board[1][1].value, board[2][0].value]);
+    if (!diag2.has(0) && diag2.size === 1) {
+        return board[1][1].value;
     }
 
     //tie
@@ -89,29 +112,26 @@ function getWinner() {
         return 'T';
     }
 
+    //no winner
     return null;
 }
 
 function render() {
     renderBoard();
     renderMessage();
-    //hide/show UI elements (controls)
     renderControls();
 }
 
 function renderBoard() {
-    board.forEach((rowArr, rowIdx) => {
-        rowArr.forEach((cellVal, colIdx) => {
-            const cellId = `c${colIdx}r${rowIdx}`;
-            const cellEl = document.getElementById(cellId);
-            // cellEl.textContent = DISPLAY[cellVal];
-
-            if (cellVal !== 0 && cellEl.children.length === 0) {
-                cellEl.classList.add('full');
+    board.forEach(rowCells => {
+        rowCells.forEach(cell => {
+            if (cell.value !== 0 && cell.element.children.length === 0) {
+                //replace hoverable class wth X/O 
+                cell.element.classList.replace('hoverable', cell.value); 
+                //add image to show for X/O icon
                 const img = document.createElement('img');
-                img.setAttribute('src', DISPLAY[cellVal][1]);
-                cellEl.appendChild(img);
-                img.style.height = '15vmin';
+                img.setAttribute('src', cell.imgURL);
+                cell.element.appendChild(img);
             }
         });
     });
@@ -121,51 +141,56 @@ function renderMessage() {
     if (winner === 'T') {
         messageEl.innerText = "It's a Tie!";
     } else if (winner) {
-        messageEl.innerHTML = `${DISPLAY[winner][0]} Wins!`;
+        messageEl.innerHTML = `${winner} Wins!`;
     } else {
-        messageEl.innerHTML = `Turn: ${DISPLAY[turn][0]}`;
+        messageEl.innerHTML = `Turn: <span id="turnGlow">${turn}</span>`;
     }
 }
 
 function renderControls() {
+    //if the game is over make sure no cells are clickable or hoverable
     if (winner) {
+        board.forEach(rowCells => {
+            rowCells.forEach(cell => {
+                if (cell.value === 0) {
+                    cell.element.classList.remove('hoverable');
+                }
+            });
+        });
+        //make button visible
         playAgainBtn.style.visibility = 'visible';
-        const whosTurnImgs = [...document.getElementsByClassName('whosTurn')]
-        whosTurnImgs.forEach(whosTurnImg => {
-            whosTurnImg.style.visibility = 'hidden';
-            whosTurnImg.style.height = '15vmin';
-        });
-        [...document.querySelectorAll('.tile')].forEach(tile => {
-            tile.classList.remove('tile');
-        });
+        //hide turn indicator icons
+        xIconEl.style.visibility = 'hidden';
+        xIconEl.style.height = '15vmin';
+        oIconEl.style.visibility = 'hidden';
+        oIconEl.style.height = '15vmin';
 
     } else {
+        //keep button hidden and show turn indicator icons 
         playAgainBtn.style.visibility = 'hidden';
-        const whosTurnImgs = [...document.getElementsByClassName('whosTurn')]
-        whosTurnImgs.forEach(whosTurnImg => {
-            whosTurnImg.style.visibility = 'visible';
-        });
-        [...document.querySelectorAll('#board > div')].forEach(tile => {
-            tile.classList.add('tile');
-        });
+        xIconEl.style.visibility = 'visible';
+        oIconEl.style.visibility = 'visible';
+ 
+        //ake the X/O icon whose turn it is larger and brighter
         if (turn === 'X') {
-            whosTurnImgs[0].style.height = '30vmin';
-            whosTurnImgs[0].style.margin = '-25vmin 0';
-            whosTurnImgs[1].style.height = '15vmin';
-            whosTurnImgs[1].style.margin = '-10vmin 0';
-            whosTurnImgs[1].style.opacity = '50%';
-            whosTurnImgs[0].style.opacity = '100%';
-            whosTurnImgs[0].classList.add('currTurn');
-            whosTurnImgs[1].classList.remove('currTurn');
+            xIconEl.style.height = '30vmin';
+            xIconEl.style.margin = '-25vmin 0';
+            oIconEl.style.height = '15vmin';
+            oIconEl.style.margin = '-10vmin 0';
+            oIconEl.style.opacity = '50%';
+            xIconEl.style.opacity = '100%';
+            xIconEl.classList.add('currTurn');
+            xIconEl.classList.add('animate__animated', 'animate__pulse');
+            oIconEl.classList.remove('currTurn');
         } else {
-            whosTurnImgs[1].style.height = '30vmin';
-            whosTurnImgs[1].style.margin = '-25vmin 0';
-            whosTurnImgs[0].style.height = '15vmin';
-            whosTurnImgs[0].style.margin = '-10vmin 0';
-            whosTurnImgs[0].style.opacity = '50%';
-            whosTurnImgs[1].style.opacity = '100%';
-            whosTurnImgs[1].classList.add('currTurn');
-            whosTurnImgs[0].classList.remove('currTurn');
+            oIconEl.style.height = '30vmin';
+            oIconEl.style.margin = '-25vmin 0';
+            xIconEl.style.height = '15vmin';
+            xIconEl.style.margin = '-10vmin 0';
+            xIconEl.style.opacity = '50%';
+            oIconEl.style.opacity = '100%';
+            oIconEl.classList.add('currTurn');
+            xIconEl.classList.remove('currTurn');
         }
     }
 }
